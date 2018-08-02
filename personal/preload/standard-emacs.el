19d21195a25ef81435d00088dbb07f2df5cdc3b7
Philippe Bernery <pbernery@semeia.io>
2018-08-02 17:13:00 +0200

    Set Hack font, and some helpers to change to a variable width font

--- personal/preload/standard-emacs.el
+++ personal/preload/standard-emacs.el
@@ -8,9 +8,25 @@
 ;; Window size
 (when window-system (set-frame-size (selected-frame) 104 58))
 
-;; Appearance (works with emacs-plus)
+;; Frame appearance (works with emacs-plus)
 (add-to-list 'default-frame-alist '(ns-transparent-titlebar . t))
 (add-to-list 'default-frame-alist '(ns-appearance . dark))
+
+;; Fonts
+(defun my-buffer-face-mode-variable ()
+  "Set font to a variable width (proportional) fonts in current buffer"
+  (interactive)
+  (setq buffer-face-mode-face '(:family "Cochin" :height 165))
+  (buffer-face-mode))
+
+(defun my-buffer-face-mode-fixed ()
+  "Sets a fixed width (monospace) font in current buffer"
+  (interactive)
+  (setq buffer-face-mode-face '(:family "Hack" :height 120))
+  (buffer-face-mode))
+
+;; (add-hook 'text-mode-hook 'my-buffer-face-mode-variable)
+(add-hook 'prog-mode-hook 'my-buffer-face-mode-fixed)
 
 ;; macOS specific
 (setq-default mac-right-option-modifier nil)

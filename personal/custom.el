88a06ba628f0125d9f3b95a92b037f2082e971b0
Philippe Bernery <pbernery@semeia.io>
2018-09-04 11:27:56 +0200

    Add API Blueprint support

--- personal/custom.el
+++ personal/custom.el
@@ -6,6 +6,7 @@
 (prelude-require-packages '(ag
                             all-the-icons
                             all-the-icons-ivy
+                            apib-mode
                             company-tern
                             edit-indirect
                             ember-mode
@@ -28,6 +29,9 @@
 ;;;;
 ;; PACKAGE CONFIGURATION
 ;;;;
+
+;;; apib-mode
+(add-to-list 'auto-mode-alist '("\\.apib\\'" . apib-mode))
 
 ;;; emacs-refactor
 (define-key prog-mode-map (kbd "M-RET") 'emr-show-refactor-menu)
@@ -137,7 +141,7 @@
     ("fa2b58bb98b62c3b8cf3b6f02f058ef7827a8e497125de0254f56e373abee088" default)))
  '(package-selected-packages
    (quote
-    (all-the-icons-ivy helm-core helm-projectile spacemacs-theme edit-indirect a anaconda-mode avy company ghub git-commit projectile pythonic web-mode ruby-refactor emr ag counsel helm ivy swiper dockerfile-mode js2-refactor company-tern magit-todos markdown-mode spacemacs-themes zop-to-char zenburn-theme which-key volatile-highlights undo-tree smartrep smartparens smart-mode-line operate-on-number move-text magit imenu-anywhere ibuffer-projectile hl-todo guru-mode grizzl god-mode gitignore-mode gitconfig-mode git-timemachine gist flycheck expand-region exec-path-from-shell ember-mode editorconfig easy-kill discover-my-major diminish diff-hl crux browse-kill-ring beacon anzu ace-window)))
+    (apib-mode all-the-icons-ivy helm-core helm-projectile spacemacs-theme edit-indirect a anaconda-mode avy company ghub git-commit projectile pythonic web-mode ruby-refactor emr ag counsel helm ivy swiper dockerfile-mode js2-refactor company-tern magit-todos markdown-mode spacemacs-themes zop-to-char zenburn-theme which-key volatile-highlights undo-tree smartrep smartparens smart-mode-line operate-on-number move-text magit imenu-anywhere ibuffer-projectile hl-todo guru-mode grizzl god-mode gitignore-mode gitconfig-mode git-timemachine gist flycheck expand-region exec-path-from-shell ember-mode editorconfig easy-kill discover-my-major diminish diff-hl crux browse-kill-ring beacon anzu ace-window)))
  '(safe-local-variable-values
    (quote
     ((eval setq js-switch-indent-offset 0)

12b167f26cb517a36ad1ffade350ffcf4c9d3287
Philippe Bernery <pbernery@semeia.io>
2018-09-14 17:44:16 +0200

    Add tide

--- personal/custom.el
+++ personal/custom.el
@@ -16,7 +16,8 @@
                             magit-todos
                             neotree
                             ruby-refactor
-                            spacemacs-theme))
+                            spacemacs-theme
+                            tide))
 
 ;; Disable Guru mode
 (setq prelude-guru nil)
@@ -113,6 +114,28 @@
 
 ;; ruby-refactor
 (add-hook 'ruby-mode-hook 'ruby-refactor-mode-launch)
+
+;;;; tide
+(defun setup-tide-mode ()
+  (interactive)
+  (tide-setup)
+  (flycheck-mode +1)
+  (setq flycheck-check-syntax-automatically '(save mode-enabled))
+  (eldoc-mode +1)
+  (tide-hl-identifier-mode +1)
+  ;; company is an optional dependency. You have to
+  ;; install it separately via package-install
+  ;; `M-x package-install [ret] company`
+  (company-mode +1))
+
+;; aligns annotation to the right hand side
+(setq company-tooltip-align-annotations t)
+
+(add-hook 'js2-mode-hook #'setup-tide-mode)
+
+;; configure javascript-tide checker to run after your default javascript checker
+(with-eval-after-load 'tide-mode
+  (flycheck-add-next-checker 'javascript-eslint 'javascript-tide 'append))
 
 ;;;; web-mode
 (add-to-list 'auto-mode-alist '("\\.hbs\\'" . web-mode))
